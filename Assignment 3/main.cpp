cb0f3f4f94e5e8b229ee1e7a2243ec55b00a7f38
Mick-Leinbach <cogsturner@gmail.com>
2024-03-21 15:35:18 -0500

    Stupid functions

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -24,11 +24,28 @@ public:
 		: tail(tail), front(front), num_items(num_items) {};
 
 	/*
+	@ returns size of the Queue (the number of items in it)
+	*/
+	int size() {
+		return num_items;
+	}
+
+	/*
+	@ returns true if the Queue is empty and false otherwise
+	*/
+	bool empty() {
+		if (tail == nullptr) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
 	Print function -- this function prints out the contents of the queue
 	*/
 	void print() {
 		// If the queue is empty...
-		if (tail == nullptr) {
+		if (empty()) {
 			cout << "The queue is empty." << endl;
 		}
 		// If the queue has exactly one node...
@@ -43,7 +60,7 @@ public:
 				cout << currentNode->data << ", ";
 				currentNode = currentNode->next;
 			}
-			cout << currentNode->data << " (" << num_items << " items)" << endl;
+			cout << currentNode->data << " (" << size() << " items)" << endl;
 		}
 	}
 
@@ -54,7 +71,7 @@ public:
 	void push(itemType item) {
 		Node<itemType>* newNode = new Node<itemType>(item);
 		// If the queue is empty...
-		if (tail == nullptr) {
+		if (empty()) {
 			tail = newNode;
 			front = newNode;
 			++num_items;
@@ -71,7 +88,6 @@ public:
 			tail = newNode;
 			++num_items;
 		}
-		cout << "Adding item " << newNode->data << "..." << endl;
 	}
 
 	/*
@@ -81,24 +97,138 @@ public:
 		return front->data;
 	}
 
+	/*
+	Removes the node at the front of the queue.
+	*/
+	void pop() {
+		// If the Queue is empty...
+		if (empty()) {
+			cout << "Queue empty -- No items to remove." << endl;
+		}
+		// If there is exactly one item in the Queue...
+		else if (tail == front) {
+			delete tail;
+			tail = nullptr;
+			front = nullptr;
+			--num_items;
+		}
+		else {
+		// There are multiple items in the Queue 
+			Node<itemType>* currentNode = tail;
+			while (currentNode->next != front) {
+				currentNode = currentNode->next;
+			}
+			delete front;
+			front = currentNode;
+			--num_items;
+		}
+	}
+
+	/*
+	Removes item at the front of the Queue and appends it to the back of the Queue
+	*/
+	void move_to_rear() {
+		itemType temp = getFront();
+		pop();
+		push(temp);
+	}
+
+	/*
+	Recursively searches the queue to find the last instance of a given value
+	@ param target: By-reference itemType. This is the value we're looking for the last instance of.
+	@ param pos: integer. We'll want to enter it size() - 1 when we originally call it. 
+	@ return position of the last instance of the target value.
+	*/
+	itemType reverse_linear_search(itemType target, int pos) {
+		Node<itemType>* currentNode = tail;
+		int index = 0;
+
+		while (index < pos) {
+			currentNode = currentNode->next;
+			++index;
+		}
+
+		// If the whole Queue has been searched to no avail
+		if (pos < 0)
+			return -1;
+		// If the target is found...
+		if (target == currentNode->data)
+			return pos;
+		// If the target hasn't been found but the search isn't over...
+		else
+			return reverse_linear_search(target, pos-1);
+	}
 };
 
 int main() {
 	//Variables
 	Queue<int> myQueue;
+	Queue<int> tempQueue;
 
 	// Main program
 	myQueue.print();
+	cout << endl;
 	
 	// Push function demonstration
-	cout << "Ten items will be added to the queue." << endl << endl;
+	cout << "Ten items will be added to the queue." << endl;
 	for (size_t i = 0; i < 10; ++i) {
 		myQueue.push(i * 10);
+		cout << "Adding item " << i * 10 << "..." << endl;
 		myQueue.print();
 	}
 	cout << endl;
 
 	// Front function demonstration
-	cout << "The item at the front of the queue is " << myQueue.getFront();
+	cout << "The item at the front of the queue is " << myQueue.getFront() << endl << endl;
 
+	// Pop function demonstration
+	cout << "The frontmost item will be removed from the queue ten times." << endl;
+	for (size_t i = 0; i < 11; ++i) {
+		if (!myQueue.empty()) {
+			cout << "Removing " << myQueue.getFront() << "..." << endl;
+		}
+		myQueue.pop();
+		myQueue.print();
+	}
+	cout << endl;
+	cout << "empty() and size() were both implemented and used in the code of the other functions." << endl << endl;
+
+	// Push function demonstration
+	cout << "Adding ten items to the queue." << endl;
+	for (size_t i = 0; i < 10; ++i) {
+		myQueue.push(i % 3);
+	}
+	cout << endl;
+
+	// Display with only primitive functions
+	
+	cout << "Printing Queue using only primitive functions (result is inverted)..." << endl;
+
+		// Print Queue
+		cout << "Queue: ";
+		for (size_t i = myQueue.size(); i > 1; --i) {
+			cout << myQueue.getFront() << ", ";
+			tempQueue.push(myQueue.getFront());
+			myQueue.pop();
+		}
+		cout << myQueue.getFront() << endl;
+		tempQueue.push(myQueue.getFront());
+		myQueue.pop();
+
+		// Refill original Queue
+		for (size_t i = tempQueue.size(); i > 1; --i) {
+			myQueue.push(tempQueue.getFront());
+			tempQueue.pop();
+		}
+		myQueue.push(tempQueue.getFront());
+		tempQueue.pop();
+
+	// demonstrate move_to_rear() function
+	cout << endl << "Performing move_to_rear() function..." << endl;
+	myQueue.move_to_rear();
+	myQueue.print();
+
+	// demonstrate reverse_linear_search() function
+	cout << "Demonstrating recursive search function." << endl;
+	cout << "The last instance of the value 2 is at index " << myQueue.reverse_linear_search(2, myQueue.size() - 1) << endl << endl;
 }
